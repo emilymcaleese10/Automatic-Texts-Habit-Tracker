98ca9ac8910eac80e638183136257721d35fe1ac
emilymcaleese10 <103384151+emilymcaleese10@users.noreply.github.com>
2025-02-13 14:50:02 +0000

    Added time elapse before next log

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,11 +2,13 @@
 
 from telegram import Update
 from telegram.ext import Application, MessageHandler, CallbackContext, filters
-import logging
 import json
+import time
+from datetime import datetime
 
 TOKEN = "7903410355:AAFc88HhZtqvfZ3rGzKRCZUJMUwVZFaWiqU"
 LOG_FILE = "conversations.log"
+ELAPSED_TIME_FOR_NEXT_LOG = 6*3600 # 6 hours in seconds
 
 try: 
     with open(LOG_FILE, "r", encoding="utf-8") as f:
@@ -15,12 +17,8 @@ except (FileNotFoundError, json.JSONDecodeError):
     user_conversations = {}
 
 
-async def store_message(user_id: str, user_name: str, user_message: str, bot_message: str, timestamp: str, user_conversations: dict, log_file: str):
-    user_key = f"{user_name}_{user_id}"
+async def store_message(user_key: str, user_id: str, user_name: str, user_message: str, bot_message: str, timestamp: str, user_conversations: dict, log_file: str):
 
-    if user_key not in user_conversations:
-        user_conversations[user_key] = {"total_sessions_logged": 0, "conversation_stream": []}  # Create a new conversation dictionary for the user if it doesn't exist   
-  
     user_conversations[user_key]["conversation_stream"].append({"User": user_message, "Timestamp": timestamp})  # user message
     user_conversations[user_key]["conversation_stream"].append({"Bot": bot_message}) # bot message
 
@@ -28,22 +26,42 @@ async def store_message(user_id: str, user_name: str, user_message: str, bot_mes
         json.dump(user_conversations, f, indent=4, ensure_ascii=False)  
 
 
+async def ready_to_update_counter(user_key: str) -> bool:
+    if user_conversations[user_key]["total_sessions_logged"] == 0:
+        return True
+    
+    # return true if 6 hours have passed since last log 
+    current_time = time.time()
+    last_log_time = user_conversations[user_key]["last_log_time"]
+    return (current_time - last_log_time >= 20) # CHANGE TO ELAPSED_TIME_FOR_NEXT_LOG
 
-async def generate_bot_message(user_id: str, user_name: str, user_message: str, user_conversations: dict) -> str:
-    user_key = f"{user_name}_{user_id}"
 
-    if user_key not in user_conversations:
-        user_conversations[user_key] = {"total_sessions_logged": 0, "conversation_stream": []}  # Create a new conversation dictionary for the user if it doesn't exist   
-  
+
+async def generate_bot_message(user_key: str, user_id: str, user_name: str, user_message: str, user_conversations: dict) -> str:
+    current_time = time.time()
+
+    ready_to_log = await ready_to_update_counter(user_key)
+
+    six_hours_later = current_time + ELAPSED_TIME_FOR_NEXT_LOG
+
+    # Convert to a human-readable format
+    formatted_time = datetime.fromtimestamp(six_hours_later).strftime('%H:%M %Y/%m/%d')
+
     if user_message.upper() == "GYM":
-        user_conversations[user_key]["total_sessions_logged"] += 1
-        session_count = user_conversations[user_key]["total_sessions_logged"]
-        return f"You have successfully logged a gym session! 🤸\n\nNumber of gym sessions logged: {session_count}"
+        if ready_to_log:       
+            user_conversations[user_key]["total_sessions_logged"] += 1
+            total_session_count = user_conversations[user_key]["total_sessions_logged"]
+
+            user_conversations[user_key]["logs_until_reward"] -= 1
+            logs_until_reward = user_conversations[user_key]["logs_until_reward"]
+
+            user_conversations[user_key]["last_log_time"] = current_time
+            return f"You have successfully logged a gym session! 🤸\n\nTotal number of gym sessions logged: {total_session_count}\n\nSessions left to log this week before reward: {logs_until_reward}"
+        else: 
+            return f"You have recently logged a gym session. \nNext time you can log a session is: \n{formatted_time}."
     else:
         return "send GYM to log a gym session"
-
-
-
+    
 
 async def handle_message(update: Update, context: CallbackContext) -> None:
     global user_conversations
@@ -53,8 +71,13 @@ async def handle_message(update: Update, context: CallbackContext) -> None:
     user_message = update.message.text.strip()
     timestamp = str(update.message.date)
 
-    bot_message = await generate_bot_message(user_id, user_name, user_message, user_conversations)
-    await store_message(user_id, user_name, user_message, bot_message, timestamp, user_conversations, LOG_FILE)
+    user_key = f"{user_name}_{user_id}"
+
+    if user_key not in user_conversations:
+        user_conversations[user_key] = {"total_sessions_logged": 0, "weekly_log_goal": 2, "logs_until_reward": 2, "last_log_time": time.time(), "conversation_stream": []}  # Create a new conversation dictionary for the user if it doesn't exist   
+
+    bot_message = await generate_bot_message(user_key, user_id, user_name, user_message, user_conversations)
+    await store_message(user_key, user_id, user_name, user_message, bot_message, timestamp, user_conversations, LOG_FILE)
     
     await update.message.reply_text(bot_message)
 
@@ -65,6 +88,7 @@ async def handle_message(update: Update, context: CallbackContext) -> None:
 def main():
     app = Application.builder().token(TOKEN).build()
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message)) # Add a message handler to check incoming text
+    
     print("Bot is running... Press Ctrl+C to stop.")
     app.run_polling()
 
